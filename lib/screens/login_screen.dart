65cfc06ce52347657c001cd173406b37e1da5b4c
Arjun <6112arjun@gmail.com>
2023-09-02 10:34:32 +0530

    UI Changes in the Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -24,20 +24,49 @@ class LoginScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton.icon(
-            onPressed: () {
-              signInWithGoogle(ref, context);
-            },
-            icon: Image.asset(
-              'assets/images/g-logo-2.png',
-              height: 20,
-            ),
-            label: Text(
-              'Sign in with Google',
-              style: GoogleFonts.poppins(
-                  fontWeight: FontWeight.w500, fontSize: 16),
-            )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(top: 100),
+            child: Text("Live Document Editor",
+                style: GoogleFonts.poppins(
+                    color: Colors.deepPurple[400],
+                    fontWeight: FontWeight.bold,
+                    fontSize: 34)),
+          ),
+          Center(
+            child: ElevatedButton.icon(
+                onPressed: () {
+                  signInWithGoogle(ref, context);
+                },
+                icon: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    'assets/images/g-logo-2.png',
+                    height: 35,
+                  ),
+                ),
+                label: Text(
+                  'Sign in with Google',
+                  style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w500, fontSize: 27),
+                )),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text("Made by Arjun 💜",
+                    style: GoogleFonts.poppins(
+                        color: Colors.deepPurple[200],
+                        fontWeight: FontWeight.w700,
+                        fontSize: 12)),
+              ),
+            ],
+          )
+        ],
       ),
     );
   }
